65e857b34d100d23ebdf5088d0a2932ec8296204
Wen-Tien Chang <ihower@gmail.com>
2011-08-14 01:13:40 +0800

    fix list API

--- app/actions/list_action.rb
+++ app/actions/list_action.rb
@@ -2,7 +2,7 @@ class ListAction < Cramp::Action
 
   def start
     @redis = EM::Hiredis.connect("redis://localhost:6379")
-    @channel = "talkplus-1"
+    @channel = "talkplus-#{params[:id]}"
     
     @redis.smembers(@channel).callback do |value|
       puts value.inspect
